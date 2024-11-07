8577ce7f6696187586459220daaf95c501dd347f
sandyhouston <157427931+sandyhouston@users.noreply.github.com>
2024-11-07 13:54:01 +1100

    Redirect script work with subfolder structure

--- wp-content/themes/picostrap5-child-base/js/redirect﹖v=1.0.0.js
+++ wp-content/themes/picostrap5-child-base/js/redirect﹖v=1.0.0.js
@@ -1,4 +1,4 @@
- // Get the full URL
+/*  // Get the full URL
  var fullUrl = window.location.href;
  // Create a URL object
  var url = new URL(fullUrl);
@@ -8,5 +8,35 @@
  // Redirect after 5 seconds
  setTimeout(function() {
      window.location.href = siteUrl + redirectPath;
- }, 5000);
+ }, 5000); */
+
+ // Get the full URL
+var fullUrl = window.location.href;
+console.log('Full URL:', fullUrl);
+console.log('Redirect Path:', redirectPath);
+
+// Create a URL object
+var url = new URL(fullUrl);
+
+// Define the part of the path to search for
+var searchPath = '/documentation/redirect/';
+console.log('Search Path:', searchPath);
+
+// Find the index of the searchPath in the full URL
+var searchIndex = fullUrl.indexOf(searchPath);
+console.log('Search Index:', searchIndex);
+
+// Extract the prefix up to the search path
+var prefix = fullUrl.substring(0, searchIndex);
+console.log('Prefix:', prefix);
+
+// Construct the full redirect URL
+var redirectUrl = prefix + redirectPath;
+console.log('Redirect URL:', redirectUrl);
+
+// Redirect after 5 seconds
+setTimeout(function() {
+    console.log('Redirecting to:', redirectUrl);
+    //window.location.href = redirectUrl;
+}, 5000);
 
